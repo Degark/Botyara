7f537cc51872f9e010b351810ef4fb23e1835f60
Degark <106580229+Degark@users.noreply.github.com>
2024-10-08 20:42:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def send_info(message):
             bot.send_message(id, f"Hello, {name}!")
 
 def fetch_data(url):
-    response = requests.get(url, headers = {"User-Agent": "Mozilla/5.0"})
+    response = requests.get(url, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 OPR/113.0.0.0", "sec-ch-ua-platform": "Windows"})
     soup = BeautifulSoup(response.text, "html.parser")
     list_elements = soup.find_all("div", class_ = "board-text")
 
